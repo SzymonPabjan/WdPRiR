c91cc2c45cfdd5d3d0550f892e28278a8c22edc9
Szymon P <spabjan2@gmail.com>
2021-12-09 13:27:50 +0100

    Zad 3 final code

--- src/Zad3/Zad3MultiThread.java
+++ src/Zad3/Zad3MultiThread.java
@@ -6,6 +6,8 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -39,6 +41,76 @@ public class Zad3MultiThread {
             return true;
         }
     }
+
+    public static class DownloadAndFilterPNG implements Callable {
+        URL url;
+        String fileName;
+        double sigma;
+        int kernelRadius;
+
+        public DownloadAndFilterPNG(String urlOut, String fileNameOut, double sigmaOut, int kernelRadiusOut) throws MalformedURLException {
+            this.url = new URL(urlOut);
+            this.fileName = fileNameOut;
+            this.sigma = sigmaOut;
+            this.kernelRadius = kernelRadiusOut;
+        }
+        @Override
+        public Boolean call() throws Exception {
+            BufferedImage image = ImageIO.read(url);
+            image = returnFilteredImage(image, sigma, kernelRadius);
+            ImageIO.write(image, "png", new File(fileName));
+            return true;
+        }
+    }
+    public static double[][] returnKernel(double sigma, int radius) {
+        double[][] kernel = new double[2*radius + 1][2*radius + 1];
+        double sum = 0;
+        for(int x = 0; x<2*radius + 1; x++) {
+            for(int y = 0; y<2*radius + 1; y++) {
+                double temporaryValue = Math.exp(- (Math.pow(x-radius, 2) + Math.pow(y-radius, 2)) / (2 * sigma * sigma));
+                kernel[x][y] = temporaryValue;
+                sum += temporaryValue;
+            }
+        }
+        for(int x = 0; x<2*radius + 1; x++) {
+            for (int y = 0; y < 2 * radius + 1; y++) {
+                kernel[x][y] /= sum;
+            }
+        }
+
+        return kernel;
+    }
+
+    private static BufferedImage returnFilteredImage(BufferedImage imageToProcess, double sigma, int kernelRadius) {
+        int imageToProcessWidth = imageToProcess.getWidth();
+        int imageToProcessHeight = imageToProcess.getHeight();
+        BufferedImage processedImage = new BufferedImage(imageToProcessWidth, imageToProcessHeight, imageToProcess.getType());
+        double[][] kernel = returnKernel(sigma, kernelRadius);
+        float[] tempColors = {0, 0, 0};
+
+        for(int x = 0; x<imageToProcessWidth; x++) {
+            for(int y = 0; y<imageToProcessHeight; y++) {
+                if(x-kernelRadius < 0 || x+kernelRadius >= imageToProcessWidth ||
+                        y-kernelRadius < 0 || y+kernelRadius >= imageToProcessHeight)
+                    processedImage.setRGB(x, y, imageToProcess.getRGB(x, y));
+                else {
+                    float[] newColors = {0, 0, 0};
+                    for(int xx = x-kernelRadius; xx<=x+kernelRadius; xx++) {
+                        for(int yy = y-kernelRadius; yy<=y+kernelRadius; yy++) {
+                            for(int c = 0; c<3; c++) {
+                                new Color(imageToProcess.getRGB(xx, yy)).getRGBColorComponents(tempColors);
+                                newColors[c] += tempColors[c] * kernel[xx-x+kernelRadius][yy-y+kernelRadius];
+                            }
+                        }
+                    }
+                    processedImage.setRGB(x, y, new Color(newColors[0], newColors[1], newColors[2]).getRGB());
+                }
+            }
+        }
+
+        return processedImage;
+    }
+
     public static void downloadAndSavePNGs() throws IOException, InterruptedException {
         String url = "http://www.if.pw.edu.pl/~mrow/dyd/wdprir/";
         Document doc = Jsoup.connect(url).get();
@@ -52,12 +124,32 @@ public class Zad3MultiThread {
         long start = System.nanoTime();
         List<Future<Boolean>> futures = executor.invokeAll(callableList);
         long end = System.nanoTime();
+        executor.shutdown();
         System.out.println("Czas pobierania wielowątkowego w sekundach: " + ((end - start)/(Math.pow(10,9))));
+        System.out.println("Done downloading");
+    }
+
+    public static void downloadAndFilterAndSavePNGs(double sigma, int kernelRadius) throws IOException, InterruptedException {
+        String url = "http://www.if.pw.edu.pl/~mrow/dyd/wdprir/";
+        Document doc = Jsoup.connect(url).get();
+        Elements pngs = doc.select("a[href*=.png]");
+        ArrayList<Callable<Boolean>> callableList = new ArrayList<>();
+        for(int i = 0; i<pngs.size(); i++ ){
+            callableList.add(new DownloadAndFilterPNG((url +"/"+pngs.get(i).attr("href")),pngs.get(i).attr("href"), sigma, kernelRadius));
+        }
+        ExecutorService executor = Executors.newFixedThreadPool(8);
+        System.out.println("Zaczynam wielowątkowe pobieranie i filtrowanie obrazów z " + url);
+        long start = System.nanoTime();
+        List<Future<Boolean>> futures = executor.invokeAll(callableList);
+        long end = System.nanoTime();
+        executor.shutdown();
+        System.out.println("Czas pobierania wielowątkowego i filtrowania w sekundach: " + ((end - start)/(Math.pow(10,9))));
         System.out.println("Done downloading");
     }
 
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        downloadAndSavePNGs();
+        //downloadAndSavePNGs();
+        downloadAndFilterAndSavePNGs(1,1);
     }
 }
--- src/Zad3/Zad3Sequential.java
+++ src/Zad3/Zad3Sequential.java
@@ -4,18 +4,64 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import org.jsoup.Connection;
+import java.net.URL;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 public class Zad3Sequential {
+
+    public static double[][] returnKernel(double sigma, int radius) {
+        double[][] kernel = new double[2*radius + 1][2*radius + 1];
+        double sum = 0;
+        for(int x = 0; x<2*radius + 1; x++) {
+            for(int y = 0; y<2*radius + 1; y++) {
+                double temporaryValue = Math.exp(- (Math.pow(x-radius, 2) + Math.pow(y-radius, 2)) / (2 * sigma * sigma));
+                kernel[x][y] = temporaryValue;
+                sum += temporaryValue;
+            }
+        }
+        for(int x = 0; x<2*radius + 1; x++) {
+            for (int y = 0; y < 2 * radius + 1; y++) {
+                kernel[x][y] /= sum;
+            }
+        }
+
+        return kernel;
+    }
+
+    private static BufferedImage returnFilteredImage(BufferedImage imageToProcess, double sigma, int kernelRadius) {
+        int imageToProcessWidth = imageToProcess.getWidth();
+        int imageToProcessHeight = imageToProcess.getHeight();
+        BufferedImage processedImage = new BufferedImage(imageToProcessWidth, imageToProcessHeight, imageToProcess.getType());
+        double[][] kernel = returnKernel(sigma, kernelRadius);
+        float[] tempColors = {0, 0, 0};
+
+        for(int x = 0; x<imageToProcessWidth; x++) {
+            for(int y = 0; y<imageToProcessHeight; y++) {
+                if(x-kernelRadius < 0 || x+kernelRadius >= imageToProcessWidth ||
+                        y-kernelRadius < 0 || y+kernelRadius >= imageToProcessHeight)
+                    processedImage.setRGB(x, y, imageToProcess.getRGB(x, y));
+                else {
+                    float[] newColors = {0, 0, 0};
+                    for(int xx = x-kernelRadius; xx<=x+kernelRadius; xx++) {
+                        for(int yy = y-kernelRadius; yy<=y+kernelRadius; yy++) {
+                            for(int c = 0; c<3; ++c) {
+                                new Color(imageToProcess.getRGB(xx, yy)).getRGBColorComponents(tempColors);
+                                newColors[c] += tempColors[c] * kernel[xx-x+kernelRadius][yy-y+kernelRadius];
+                            }
+                        }
+                    }
+                    processedImage.setRGB(x, y, new Color(newColors[0], newColors[1], newColors[2]).getRGB());
+                }
+            }
+        }
+
+        return processedImage;
+    }
+
+
     public static void downloadAndSavePNGs() throws IOException {
         String url = "http://www.if.pw.edu.pl/~mrow/dyd/wdprir/";
         Document doc = Jsoup.connect(url).get();
@@ -23,13 +69,43 @@ public class Zad3Sequential {
         System.out.println("Zaczynam pobieranie obrazów z " + url);
         long start = System.nanoTime();
         for(int i = 0; i<pngs.size(); i++ ){
-            //System.out.println(url +"/"+pngs.get(i).attr("href"));
-            Connection.Response resultImageResponse = Jsoup.connect(url +"/"+pngs.get(i).attr("href")).maxBodySize(0)
-                    .ignoreContentType(true).execute();
-            FileOutputStream out = (new FileOutputStream(new java.io.File(pngs.get(i).attr("href"))));
-            out.write(resultImageResponse.bodyAsBytes());
-            out.close();
+            URL pngURL = new URL(url +"/"+pngs.get(i).attr("href"));
+            ImageIO.write(ImageIO.read(pngURL), "png", new File(String.valueOf(pngs.get(i).attr("href"))));
         }
+        long end = System.nanoTime();
+        System.out.println("Czas pobierania sekwencyjnego w sekundach: " + ((end - start)/(Math.pow(10,9))));
+        System.out.println("Done downloading");
+    }
+
+    public static void downloadAndFilterAndSavePNGs(double sigma, int kernelRadius) throws IOException {
+        String url = "http://www.if.pw.edu.pl/~mrow/dyd/wdprir/";
+        Document doc = Jsoup.connect(url).get();
+        Elements pngs = doc.select("a[href*=.png]");
+        System.out.println("Zaczynam pobieranie i filtrowanie obrazów z " + url);
+        long start = System.nanoTime();
+        for(int i = 0; i<pngs.size(); i++ ){
+            URL pngURL = new URL(url +"/"+pngs.get(i).attr("href"));
+            BufferedImage image = ImageIO.read(pngURL);
+            image = returnFilteredImage(image, sigma, kernelRadius);
+            ImageIO.write(image, "png", new File(pngs.get(i).attr("href")));
+        }
+        long end = System.nanoTime();
+        System.out.println("Czas pobierania sekwencyjnego i filtrowania w sekundach: " + ((end - start)/(Math.pow(10,9))));
+        System.out.println("Done downloading");
+    }
+
+    //funkcja do testowania filtra
+    public static void downloadAndFilterAndSaveOnePNG(double sigma, int kernelRadius) throws IOException {
+        String url = "http://www.if.pw.edu.pl/~mrow/dyd/wdprir/";
+        Document doc = Jsoup.connect(url).get();
+        Elements pngs = doc.select("a[href*=.png]");
+        System.out.println("Zaczynam pobieranie i filtrowanie obrazów z " + url);
+        long start = System.nanoTime();
+        URL pngURL = new URL(url +"/"+pngs.get(1).attr("href"));
+        BufferedImage image = ImageIO.read(pngURL);
+        image = returnFilteredImage(image, sigma, kernelRadius);
+        ImageIO.write(image, "png", new File(pngs.get(1).attr("href")));
+
         long end = System.nanoTime();
         System.out.println("Czas pobierania sekwencyjnego w sekundach: " + ((end - start)/(Math.pow(10,9))));
         System.out.println("Done downloading");
@@ -37,6 +113,8 @@ public class Zad3Sequential {
 
 
     public static void main(String[] args) throws IOException {
-        downloadAndSavePNGs();
+        //downloadAndSavePNGs();
+        //downloadAndFilterAndSavePNGs(1,3);
+        downloadAndFilterAndSaveOnePNG(7,2);
     }
 }
